bdd8190cfe95eea8ac9bd6336614f185a3037c24
Teodor Heggelund <git@teod.eu>
2025-03-10 18:31:17 +0100

    Grokking vars: the key to making iteractive, long-running systems

---
+++ notebooks/towards_interactive_programming/grokking_vars.clj
@@ -0,0 +1,6 @@
+;; # Grokking vars: the key to turning long-running processes interactive
+
+(ns towards-interactive-programming.grokking-vars)
+
+;; If you're not using vars in your Clojure programming practice, you're missing out.
+;; I'm telling you because it took me a long time to grok vars, and I was missing out.
